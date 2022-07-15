dbc8dd24025fbb4404b26e0fb5bdb1d12848647d
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2022-07-15 15:07:34 +0100

    add function to unlock and lock recipes

--- src/components/CollapsibleRecipes/index.js
+++ src/components/CollapsibleRecipes/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from "react-router-dom";
 
@@ -10,28 +10,109 @@ import './style.css'
 import Collapsible from 'react-collapsible';
 
 
-const CollapsibleRecipes = ({recipes, triggerName}) => {
+const CollapsibleRecipes = ({recipes, triggerName, setRecipes}) => {
   const dispatch = useDispatch()
   const navigate = useNavigate();
   
   const viewFullRecipe = (e) => {
-    const newRecipeId = e.target.parentElement.id
+    const newRecipeId = e.target.parentElement.className
+    console.log(newRecipeId)
     dispatch({ type: "SET RECIPE ID", payload: newRecipeId})
     navigate("/recipe")
   }
+console.log(recipes[0].id)
+
+  const [mealRecipes, setMealRecipes] = useState(recipes)
+  // console.log(mealRecipes[0].lock)
+  const unlockRecipe = (e) => {
+    const parentClassName = e.target.parentElement.className
+    const splitString = parentClassName.split(' ')
+    const lockRecipeIdStr = splitString[1]
+    const lockRecipeIdInt = parseInt(lockRecipeIdStr)
+    // const result = words.filter(word => word.length > 6);
+    for (let i = 0; i < recipes.length; i ++){
+      if (mealRecipes[i].id === lockRecipeIdInt){
+      console.log("found yah")
+      console.log(mealRecipes[i].lock)
+      mealRecipes[i].lock = false
+      console.log(mealRecipes[i].lock)
+      setMealRecipes(mealRecipes)
+    }
+    }
+  }
+  const changeLockRecipe = (e) => {
+    const parentClassName = e.target.parentElement.className
+    const lockClassName = e.target.className
+    console.log("lock class name", lockClassName)
+    const splitString = parentClassName.split(' ')
+    const lockRecipeIdStr = splitString[1]
+    const lockRecipeIdInt = parseInt(lockRecipeIdStr)
+    // const result = words.filter(word => word.length > 6);
+
+    for (let i = 0; i < mealRecipes.length; i ++){
+      if (mealRecipes[i].id === lockRecipeIdInt){
+        if(lockClassName === "locked"){
+          console.log("unlock me please")
+          console.log(mealRecipes[i].lock)
+          mealRecipes[i].lock = false
+          console.log(mealRecipes[i].lock)
+          e.target.className = "unlocked"
+        }
+        if(lockClassName ===  "unlocked"){
+          console.log("lock me please")
+          console.log(mealRecipes[i].lock)
+          mealRecipes[i].lock = true
+          console.log(mealRecipes[i].lock)
+          e.target.className = "locked"
+        }
+    }
+    }
+    
+  }
+  const changefaveRecipe = (e) => {
+    const parentClassName = e.target.parentElement.className
+    const faveClassName = e.target.className
+    console.log("fave class name", faveClassName)
+    const splitString = parentClassName.split(' ')
+    const faveRecipeIdStr = splitString[1]
+    const faveRecipeIdInt = parseInt(faveRecipeIdStr)
+    // const result = words.filter(word => word.length > 6);
+
+    for (let i = 0; i < mealRecipes.length; i ++){
+      if (mealRecipes[i].id === faveRecipeIdInt){
+        if(faveClassName === "locked"){
+          console.log("unfave me please")
+          console.log(mealRecipes[i].fave)
+          mealRecipes[i].fave = false
+          console.log(mealRecipes[i].fave)
+          e.target.className = "unfaved"
+        }
+        if(faveClassName ===  "unfaved"){
+          console.log("fave me please")
+          console.log(mealRecipes[i].fave)
+          mealRecipes[i].fave = true
+          console.log(mealRecipes[i].fave)
+          e.target.className = "faved"
+        }
+    }
+    }
+  }
+ 
 
   return (
     <>
           <Collapsible trigger={triggerName}>
-            {recipes.map(recipe => {
+            {mealRecipes.map(recipe => {
               return (
-                <div className="recipe" id={recipe.id} key={recipe.id} onClick={viewFullRecipe}>
-                  <h3>{recipe.title}</h3>
-                  {recipe.image && <img src={recipe.image} alt="" />}
-                  {recipe.lock === true && <img src={closedLock} alt="" />}
-                  {recipe.lock === false && <img src={openLock} alt="" />}
-                  {recipe.fave === true && <img src={filledHeart} alt="" />}
-                  {recipe.fave === false && <img src={emptyHeart} alt="" />}
+                <div key={recipe.id} className="recipe">
+                  <div className={recipe.id} onClick={viewFullRecipe}>
+                    <h3>{recipe.title}</h3>
+                    {recipe.image && <img src={recipe.image} alt="" />}
+                  </div>
+                  <div className={"recipeIcon " + recipe.id}>
+                    {recipe.lock && <div onClick={changeLockRecipe} className="locked"/>}
+                    {recipe.fave && <div onClick={changefaveRecipe} className="unfaved"/>}
+                  </div>
                 </div>
               )
             })}
--- src/pages/MealPlan/index.js
+++ src/pages/MealPlan/index.js
@@ -318,8 +318,8 @@ const MealPlan = () => {
                 }
             ],
             "summary": "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs might be a good recipe to expand your main course repertoire. One portion of this dish contains approximately <b>19g of protein </b>,  <b>20g of fat </b>, and a total of  <b>584 calories </b>. For  <b>$1.63 per serving </b>, this recipe  <b>covers 23% </b> of your daily requirements of vitamins and minerals. This recipe serves 2. It is brought to you by fullbellysisters.blogspot.com. 209 people were glad they tried this recipe. A mixture of scallions, salt and pepper, white wine, and a handful of other ingredients are all it takes to make this recipe so scrumptious. From preparation to the plate, this recipe takes approximately  <b>45 minutes </b>. All things considered, we decided this recipe  <b>deserves a spoonacular score of 83% </b>. This score is awesome. If you like this recipe, take a look at these similar recipes: <a href=\"https://spoonacular.com/recipes/cauliflower-gratin-with-garlic-breadcrumbs-318375\">Cauliflower Gratin with Garlic Breadcrumbs</a>, < href=\"https://spoonacular.com/recipes/pasta-with-cauliflower-sausage-breadcrumbs-30437\">Pasta With Cauliflower, Sausage, & Breadcrumbs</a>, and <a href=\"https://spoonacular.com/recipes/pasta-with-roasted-cauliflower-parsley-and-breadcrumbs-30738\">Pasta With Roasted Cauliflower, Parsley, And Breadcrumbs</a>.",
-        },
-        {"lock": true}
+            "lock": true
+        }
     ])
     // const [triggerNames, setTriggerNames] = useState(["Breakfast", "Lunch", "Dinner", "Dessert", "Snacks"])
 // useEffect to call users meal plan
@@ -363,19 +363,19 @@ const MealPlan = () => {
 
         {stateRecipes.breakfast.length > 0 && (
             // will change each of these to stateRecipes.breakfast respectively
-            <CollapsibleRecipes recipes={recipes} triggerName="Breakfast"/>
+            <CollapsibleRecipes recipes={recipes} setRecipes={setRecipes} triggerName="Breakfast"/>
         )}
         {stateRecipes.lunch.length > 0 && (
-            <CollapsibleRecipes recipes={recipes} triggerName="Lunch"/>
+            <CollapsibleRecipes recipes={recipes} setRecipes={setRecipes} triggerName="Lunch"/>
         )}
         {stateRecipes.dinner.length > 0 && (
-            <CollapsibleRecipes recipes={recipes} triggerName="Dinner"/>
+            <CollapsibleRecipes recipes={recipes} setRecipes={setRecipes} triggerName="Dinner"/>
         )}
         {stateRecipes.dessert.length > 0 && (
-            <CollapsibleRecipes recipes={recipes} triggerName="Dessert"/>
+            <CollapsibleRecipes recipes={recipes} setRecipes={setRecipes} triggerName="Dessert"/>
         )}
         {stateRecipes.snack.length > 0 && (
-            <CollapsibleRecipes recipes={recipes} triggerName="Snacks"/>
+            <CollapsibleRecipes recipes={recipes} setRecipes={setRecipes} triggerName="Snacks"/>
         )}
         </div>
         </>
