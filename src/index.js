02f473e458928ca895098792623e874d9856b62d
Ali Hamed Ali <59931761+alihamedali96@users.noreply.github.com>
2022-07-20 15:09:45 +0400

    Homepage

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const apiKey = "a59cc791056a438ab4b73975ca70e20f"
 
 export default apiKey
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') || document.createElement('div'));
 root.render(
     <Provider store={store}>
       <Router>
