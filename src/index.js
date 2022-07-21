9c589b1fbfe4ad5796cbc3afc89d4373a0a473d2
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2022-07-21 17:15:05 +0100

    fix a lil bug

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { createStore } from "redux";
 import store from "./store";
 // const store = createStore(reducer)
 
-const apiKey = "6b1f02c091b4429baee72031207aa9a8";
+const apiKey = "";
 
 export default apiKey;
 
